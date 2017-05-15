3e495aeaabb2b2403abc9f00d3d9ac6946c2af03
Keven Jesus <falecom@keven.com.br>
2017-05-15 10:43:04 -0300

    plugins, arquivo package e watch geral em todos arquivos .scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require ("gulp");
-var sqwish = require('gulp-sqwish');
-var htmlmin = require('gulp-html-minifier2');
+var sqwish = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
 
 /*Tarefa "minifica-css" minifica todos os arquivos .scss da pasta "scss" e depois joga ele para a pasta "dist/css"*/
 gulp.task('minifica-css', function () {
   return gulp.src('./source/scss/*.scss') /*busca todos os arquivos .scss da pasta "source" */
-    .pipe(sqwish()) /*minifica o(s) arquivo(s)*/
+    .pipe(sass({outputStyle: "compressed"})) /*minifica o(s) arquivo(s)*/
     .pipe(gulp.dest('./dist/css')); /*envia o arquivo minificado para a pasta "dist/css" */
 });
 
@@ -19,9 +19,7 @@ gulp.task('minifica-html', function() {
 
 
 gulp.task ('background' , function(){ /*background usa função "watch" que 'escuta' qdo o arquivo é modificado (qdo é salvo)*/
-	gulp.watch('./source/scss/base.scss',['minifica-css']); /*qdo 'base.scss' é modificado, o 'minifica-css' é executado*/
-	gulp.watch('./source/scss/layout.scss',['minifica-css']); /*qdo 'layout.scss' é modificado, o 'minifica-css' é executado*/
-	gulp.watch('./source/scss/style.scss',['minifica-css']); /*qdo 'style.scss' é modificado, o 'minifica-css' é executado*/
+	gulp.watch('./source/scss/*.scss',['minifica-css']); 
 	gulp.watch('./source/index.html',['minifica-html']); /*qdo 'index.html' é modificado, o 'minifica-html' é executado*/
 });
 
